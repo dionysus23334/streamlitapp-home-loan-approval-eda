d89cfea6d8e81f84586dd6baccb23e521d05955f
Anne0513 <148938970+Anne0513@users.noreply.github.com>
2023-11-07 21:28:02 +0800

    刘天琦Update main.py

--- main.py
+++ main.py
@@ -36,13 +36,23 @@ def page_home():
 #Author:Tianqi Liu
 #To show the mean/max/min value of ApplicantIncome/CoapplicantIncome/LoanAmount under the selection of whether it is succesfully loaned.
 def page_question2():
-    apptitle = 'question2'
-    st.set_page_config(page_title=apptitle, page_icon=":eyeglasses:")
+    st.title('question2')
     info=[0,1]
     select_loan=st.selectbox('Please enter whether the applicant has successfully borrowed (0 represents unsuccessful, 1 represents successful) ',info.columns.tolist())
     df_selected_new=get_data.select_Loan_Status(select_loan)
     info2=['ApplicantIncome','CoapplicantIncome','LoanAmount']
     select_line=st.selectbox('Please enter the data you want to view',info2.columns.tolist())
+    mean=df_select_new[select_line].mean()
+    min=df_select_new[select_line].min()
+    max=df_select_new[select_line].min()
+    data = {'mean': [mean],
+        'min': [min],
+        'max': [max]}
+
+df = pd.DataFrame(data)
+
+st.write(df)
+
     
     
     
