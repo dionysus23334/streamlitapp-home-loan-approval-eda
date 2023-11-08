4ecc780a4a9f77e9beb6bbf42dc834f3d9753283
Anne0513 <148938970+Anne0513@users.noreply.github.com>
2023-11-08 22:31:34 +0800

    Update plots.py

--- pages/plots.py
+++ pages/plots.py
@@ -8,65 +8,8 @@ import matplotlib.ticker as ticker
 import pandas as pd
 import numpy as np
 import streamlit as st
-
-#Author:Yuxi Guo
-#This section is to wash the data, making it more convenient for EDA. And I use some build-in functions to pad the nan value of data we chose.
-def is_graduate(x):
-    if x=='Graduate':
-        return 1
-    else:
-        return 0
-def is_female(x):
-    if x=='Female':
-        return 1
-    else:
-        return 0
-def is_married(x):
-    if x=='Yes':
-        return 1
-    else:
-        return 0
-def is_urban(x):
-    if x=='Urban':
-        return 1
-    else:
-        return 0
-def is_self_employed(x):
-    if x=='Yes':
-        return 1
-    else:
-        return 0    
-def Loan_Status_(x):
-    if x=='Y':
-        return 1
-    else:
-        return 0
-    
-def wash_data():
-    HomeLoansApproval=pd.read_csv('loan_sanction_train.csv')
-    #drop the data that has same ID
-    HomeLoansApproval=HomeLoansApproval.drop_duplicates(subset=['Loan_ID'])
-    #use subset to point out the columns whose nan values are deleted 
-    HomeLoansApproval.dropna(axis=0,how='any',subset=['Gender','Married','Dependents','Self_Employed','Credit_History'],inplace=True)
-    HomeLoansApproval_mean=HomeLoansApproval['LoanAmount'].fillna(value=HomeLoansApproval['LoanAmount'].mean(),inplace=False)
-    #median replacing
-    HomeLoansApproval_median=HomeLoansApproval['Loan_Amount_Term'].fillna(HomeLoansApproval['Loan_Amount_Term'].median(),inplace=False)
-    #replace the old series object with new series.
-    HomeLoansApproval=HomeLoansApproval.drop(labels=['LoanAmount','Loan_Amount_Term'],axis=1)
-    HomeLoansApproval['LoanAmount']=HomeLoansApproval_mean
-    HomeLoansApproval['Loan_Amount_Term']=HomeLoansApproval_median
-    HomeLoansApproval['Is_graduate']=HomeLoansApproval['Education'].apply(lambda x:is_graduate(x))
-    HomeLoansApproval['Is_Female']=HomeLoansApproval['Gender'].apply(lambda x:is_female(x))
-    HomeLoansApproval['Is_married']=HomeLoansApproval['Married'].apply(lambda x:is_married(x))
-    HomeLoansApproval['Is_urban']=HomeLoansApproval['Property_Area'].apply(lambda x:is_urban(x))
-    HomeLoansApproval['Is_self_employed']=HomeLoansApproval['Self_Employed'].apply(lambda x:is_self_employed(x))
-    HomeLoansApproval['Loan_Status_']=HomeLoansApproval['Loan_Status'].apply(lambda x:Loan_Status_(x))
-    Loan_Status=HomeLoansApproval['Loan_Status_']
-    HomeLoansApproval=HomeLoansApproval.drop(['Education','Gender','Married','Property_Area','Self_Employed','Loan_Status','Loan_Status_'],axis=1)
-    HomeLoansApproval['Loan_Status']=Loan_Status
-    HomeLoansApproval['Dependents']=HomeLoansApproval['Dependents'].apply(lambda x:( (0 if x=='0' else 1) if x!='2' else 2 )if x!='3+' else 3)
-    return HomeLoansApproval
-
+import wash_data
+import get_data
 
 #Author:Yuxi Guo
 def data_selected():
@@ -261,6 +204,11 @@ def plot_pie_chart():
     # rending pie chart
     st_pyecharts(pie_chart)
     # 
+    a=st.button('Introduction')
+    if a:
+       st.markdown('''
+    From this data, we can observe that the highest approval rate is in semi-urban areas, followed by urban areas and then rural areas. Correspondingly, the highest failure rate is in rural areas. This suggests that individuals living in semi-urban areas have a higher likelihood of getting their home loan approved compared to those in urban or rural areas.
+Therefore, if you wants to analyze the advantages and disadvantages of your own area, you should consider the fact that living in a semi-urban area may provide a higher chance of home loan approval. However, it is important to note that this analysis is based solely on the provided data and other factors may also influence loan approval rates in different areas.''')
     return None
 
 #Author:Yuxi Guo
