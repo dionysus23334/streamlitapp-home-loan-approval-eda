1d6e4041cb3395a50cccb6eab66b155d62cd43a0
dionysus23334 <134669199+dionysus23334@users.noreply.github.com>
2023-11-08 19:34:43 +0800

    Update questions.py

--- pages/questions.py
+++ pages/questions.py
@@ -63,7 +63,7 @@ def page_question2():
     select_loan=st.selectbox('Please enter whether the applicant has successfully borrowed (0 represents unsuccessful, 1 represents successful) ',info.columns.tolist())
     df_selected_new=get_data.select_Loan_Status(select_loan)
     info2=['ApplicantIncome','CoapplicantIncome','LoanAmount']
-    select_line=st.selectbox('Please enter the data you want to view',info2.columns.tolist())
+    select_line=st.selectbox('Please enter the data you want to view',info2)
     mean=df_select_new[select_line].mean()
     min=df_select_new[select_line].min()
     max=df_select_new[select_line].min()
