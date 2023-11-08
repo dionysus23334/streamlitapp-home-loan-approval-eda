a8ca02e6c95a77c127d6bc60bb86f4b388bbe11e
dionysus23334 <134669199+dionysus23334@users.noreply.github.com>
2023-11-08 19:21:34 +0800

    Update questions.py

--- pages/questions.py
+++ pages/questions.py
@@ -5,3 +5,25 @@ import get_data
 import wash_data
 
 
+def page_question2():
+    st.title('question2')
+    info=[0,1]
+    select_loan=st.selectbox('Please enter whether the applicant has successfully borrowed (0 represents unsuccessful, 1 represents successful) ',info.columns.tolist())
+    df_selected_new=get_data.select_Loan_Status(select_loan)
+    info2=['ApplicantIncome','CoapplicantIncome','LoanAmount']
+    select_line=st.selectbox('Please enter the data you want to view',info2.columns.tolist())
+    mean=df_select_new[select_line].mean()
+    min=df_select_new[select_line].min()
+    max=df_select_new[select_line].min()
+    data = {'mean': [mean],
+        'min': [min],
+        'max': [max]}
+    df = pd.DataFrame(data)
+    st.write(df)
+
+
+
+
+
+
+
